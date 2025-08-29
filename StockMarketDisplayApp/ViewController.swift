ce936d10da9e9613e800ddeba0e9d988bafa1f61
Norbert Grover <norbert298@gmail.com>
2025-08-28 22:42:57 -0700

    Initial Commit

---
+++ StockMarketDisplayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockMarketDisplayApp
+//
+//  Created by Norbert Grover on 8/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
